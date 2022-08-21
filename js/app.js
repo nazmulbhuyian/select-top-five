fefc82d17116be9bcd74634e369d4f2bd6dd5138
unknown <nazmulnobel000@gmail.com>
2022-08-22 01:44:05 +0600

    html code done

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// document.getElementById('btn-aguaro').addEventListener('click', function(){
+//     const aguaroField = document.getElementById('aguaro');
+//     const aguaroFieldString = aguaroField.innerText;
+//     const aguaro = aguaroFieldString;
+
+
+//     const ulField = document.getElementById('player-list');
+//     const ulFieldString = ulField.innerText;
+//     const ul = ulFieldString;
+// })
