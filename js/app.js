2f2ac03d84e9f6544ec9a3d1a9794fd26763a7ef
unknown <nazmulnobel000@gmail.com>
2022-08-23 01:56:55 +0600

    even handeler add

--- js/app.js
+++ js/app.js
@@ -1,10 +1,55 @@
-// document.getElementById('btn-aguaro').addEventListener('click', function(){
-//     const aguaroField = document.getElementById('aguaro');
-//     const aguaroFieldString = aguaroField.innerText;
-//     const aguaro = aguaroFieldString;
+
+const cartArray = [];
+function addToCart(element){
+    const productName = element.parentNode.children[0].innerText;
+    const productObj = {productName: productName}
+    cartArray.push(productObj);
+    element.setAttribute("disabled", true);
+    element.style.backgroundColour  = 'gray';
+    
+    if(cartArray.length < 6){
+        display(productName);
+    }
+    else{
+        alert('plz')
+    }
+    
+}
+
+function display(cartProduct){
+    const playerList = document.getElementById('player-list');
+    playerList.innerHTML = '';
+    
+    for(i = 0; i < cartProduct.length; i++){
+        
+        const name = cartArray[i].productName;
+
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+        <li class="text-white">${name} </li>
+        `
+        playerList.appendChild(li);
+
+    }
+    
+}
 
 
-//     const ulField = document.getElementById('player-list');
-//     const ulFieldString = ulField.innerText;
-//     const ul = ulFieldString;
+
+
+// document.getElementById('btn-delete').addEventListener('click', function(){
+//     const deleteButton = document.getElementById('btn-delete');
+//     if(deleteButton === 'click'){
+//         deleteButton.removeAttribute('disabled');     
+//     }
+//     else{
+//         deleteButton.setAttribute('disabled', true);
+//     }
 // })
+
+// const makebluebutton = document.getElementsByClassName('makeblue');
+// makebluebutton.onclick = makeblue;
+// function makeblue() {
+//     document.body.style.backgroundColor = 'black';
+// }
---
+++ js/calculation.js
@@ -0,0 +1,31 @@
+document.getElementById('calculate').addEventListener('click', function(){
+    const perPlayerHead = document.getElementById('player');
+    const perPlayerHeadString = perPlayerHead.value;
+    const perPlayer = parseFloat(perPlayerHeadString);
+    
+    const totalExpencesId = document.getElementById('total-expences');
+    const totalExpencesString = totalExpencesId.innerText;
+    const totalExpencesField = parseFloat(totalExpencesString);
+
+    const ToalPlayerCoast = perPlayer * 5;
+    totalExpencesId.innerText = ToalPlayerCoast;
+
+
+    const managerInputField = document.getElementById('manager');
+    const managerInputFieldString = managerInputField.value;
+    const manager = parseFloat(managerInputFieldString);
+    
+
+    const coachInputField = document.getElementById('coach');
+    const coachInputFieldString = coachInputField.value;
+    const coach = parseFloat(coachInputFieldString);
+    
+
+    const totalCoastFieldId = document.getElementById('total-coast');
+    const totalCoastString = totalCoastFieldId.innerText;
+    const totalCoastField = parseFloat(totalCoastString);
+
+    const totalCoast = ToalPlayerCoast + manager + coach;
+    
+    totalCoastFieldId.innerText = totalCoast;
+})
